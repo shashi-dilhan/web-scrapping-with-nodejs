9967ef3955cddd0ac8b1c415d191b3e345d5c573
Shashika Dilhan <shashika2sd@gmail.com>
2016-12-07 16:19:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,34 +5,19 @@ var cheerio = require('cheerio');
 var app     = express();
 
 app.get('/', function(req, res) {
-    
     url = 'http://www.imdb.com/title/tt1229340/';
-
     request(url, function(error, response, html) {
         if(!error){
             var $ = cheerio.load(html);
-            var title, release, rating;
-            var json = { title : "", release : "", rating : ""};
-
-            // We'll use the unique header class as a starting point.
+	    var text = '';
             $('.summary_text').filter(function(){
-	           	// Let's store the data we filter into a variable so we can easily see what's going on.
-	            var data = $(this);
-
-	           	// In examining the DOM we notice that the title rests within the first child element of the header tag. 
-	            // Utilizing jQuery we can easily navigate and get the text by writing the following code:
-
-	            //title = data.children().first().text();
-	            title = data.text();
-
-           		// Once we have our title, we'll store it to the our json object.
-                json.title = title;
-                res.json(title);
+	        var data = $(this);
+	        text = data.text();
             });
+            res.json(text);
         }
     })
 })
 
 app.listen('3000')
-console.log('Magic happens on port 3000');
 exports = module.exports = app;
